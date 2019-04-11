d84a2d1ed993c51d9251a3d4cb12dcf226a9eeb6
Eugene <e.kuzmenko@dunice.net>
2019-04-11 15:33:48 +0300

    Added ability to add items

--- app/routes/home/items.js
+++ app/routes/home/items.js
@@ -2,6 +2,17 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('item');
+    return {
+      itemList: this.store.findAll('item'),
+      newItemName: ''
+    }
+  },
+  actions: {
+    createItem() {
+      const item = this.store.createRecord('item', {
+        name: this.modelFor(this.routeName).newItemName,
+      });
+      item.save();
+    }
   }
 });
