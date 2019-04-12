80760c99db7548fc1c2ed311d3e2ba1a0867cc3e
Eugene <e.kuzmenko@dunice.net>
2019-04-12 18:01:00 +0300

    Some scraps

--- app/routes/home/recipes.js
+++ app/routes/home/recipes.js
@@ -1,11 +1,18 @@
 import Route from '@ember/routing/route';
 
-
 export default Route.extend({
   model() {
-    const recipeList = this.store.findAll('recipe', { include: 'itemsIn,itemsIn.item,itemsOut,itemsOut.item'});
-    return {
-      recipeList,
-    }
+    return this.store.findAll('recipe', {
+      include: 'itemsIn,itemsIn.item,itemsOut,itemsOut.item'
+    });
   },
+
+  actions: {
+    // addRecipe() {
+    //   console.log('addRecipe');
+    // },
+    // addRatio() {
+    //   console.log('addRatio')
+    // },
+  }
 });
