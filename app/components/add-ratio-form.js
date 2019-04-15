f8a30a62287b638228f25e1c77f567432bc0e875
Eugene <42930224+Eugene-Kuzmenko@users.noreply.github.com>
2019-04-15 15:11:25 +0300

    changed Item list

--- app/components/add-ratio-form.js
+++ app/components/add-ratio-form.js
@@ -1,8 +1,14 @@
-import Component from '@ember/component';
+import {A} from "@ember/array";
+import Component from "@ember/component";
+import {inject} from "@ember/service";
 
 export default Component.extend({
+  store: inject(),
   item: null,
   isOut: false,
   qty: 0,
-  availableItems() {},
+  availableItems: A([]),
+  init() {
+    this.set("availableItems", this.store.findAll("item"));
+  }
 });
